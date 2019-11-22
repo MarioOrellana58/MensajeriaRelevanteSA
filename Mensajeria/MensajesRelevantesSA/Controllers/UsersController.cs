fed5f913c6063b4e1c80adb977f65207f03c60de
eduardotejedaleon <eduardotejedaleon@gmail.com>
2019-11-22 15:14:57 -0600

    Vista menejo de usuarios

---
+++ Mensajeria/MensajesRelevantesSA/Controllers/UsersController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MensajesRelevantesSA.Models;
+namespace MensajesRelevantesSA.Controllers
+{
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public ActionResult LogInUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LogInUser(string userName, string userPassword)
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateUser(string newName, string newPass)
+        {
+
+            return View();
+        }
+        // GET: Users/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Mensajeria/MensajesRelevantesSA/Models/UserNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MensajesRelevantesSA.Models
+{
+    public class UserNode
+    {
+        string UserName;
+        string Password;
+    }
+}
