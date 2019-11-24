2df0b8cd3ba6cd0897022fdac04de1b1b8c724cf
Mario Estuardo Gómez Orellana <mario.e.g.orellana@gmail.com>
2019-11-23 20:29:22 -0600

    Conexión a UsersDB en Mongo

--- UsersAPI/Services/UsersService.cs
+++ UsersAPI/Services/UsersService.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UsersAPI.Models;
+using MongoDB.Driver;
 
 namespace UsersAPI.Services
 {
     public class UsersService
     {
+        private readonly IMongoCollection<UserNode> _Users;
+
+        public UsersService(IUsersDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _Users = database.GetCollection<UserNode>(settings.UsersCollectionName);
+        }
     }
 }
