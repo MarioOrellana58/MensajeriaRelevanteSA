b7ffcab604a79243eb0a2c1407c8a0bf161756b0
Mario Estuardo Gómez Orellana <mario.e.g.orellana@gmail.com>
2019-11-24 07:04:46 -0600

    Correcciones bugs de API

---
+++ UsersAPI/Controllers/UsersController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsersAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UsersAPI.Models;
+
+namespace Pizza_API.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UsersService _userService;
+
+        public UsersController(UsersService usersService)
+        {
+            _userService = usersService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<UserNode>> Get() =>
+            _userService.Get();
+
+        [HttpGet("{Username}", Name = "GetUser")]
+        public ActionResult<UserNode> Get(string username)
+        {
+            var user = _userService.Get(username);
+
+            if (user == null)
+            {
+                return StatusCode(404, "Your user does not exist");
+            }
+
+            return user;
+        }
+
+        [HttpPost]
+        public ActionResult<UserNode> Create(UserNode user)
+        {
+            _userService.Create(user);
+
+            return CreatedAtRoute("GetUser", new { Username = user.Username.ToString() }, user);
+        }
+
+        [HttpPut("{Username}")]
+        public IActionResult Update(string username, UserNode userIn)
+        {
+            var user = _userService.Get(username);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            userIn.Id = user.Id;
+            _userService.Update(username, userIn);
+
+            return StatusCode(200, "Your user has been updated :D");
+        }
+
+        [HttpDelete("{Username}")]
+        public IActionResult Delete(string username)
+        {
+            var user = _userService.Get(username);
+
+            if (user == null)
+            {
+                return StatusCode(404, "Your user does not exist");
+            }
+
+            _userService.Remove(user.Username);
+
+            return StatusCode(200, "Your user has been deleted :D");
+        }
+    }
+}
--- UsersAPI/Services/UsersService.cs
+++ UsersAPI/Services/UsersService.cs
@@ -19,11 +19,11 @@ namespace UsersAPI.Services
             _Users = database.GetCollection<UserNode>(settings.UsersCollectionName);
         }
 
-        public List<UserNode> Get() =>
-        _Users.Find(user => true).ToList();
+         public List<UserNode> Get() =>
+         _Users.Find(user => true).ToList();
 
-        public UserNode Get(string Username) =>
-            _Users.Find<UserNode>(user => user.Username == Username).FirstOrDefault();
+        public UserNode Get(string username) =>
+            _Users.Find(user => user.Username == username).FirstOrDefault();
 
         public UserNode Create(UserNode user)
         {
@@ -31,13 +31,14 @@ namespace UsersAPI.Services
             return user;
         }
 
-        public void Update(string Username, UserNode userIn) =>
-            _Users.ReplaceOne(user => user.Username == Username, userIn);
+        public void Update(string username, UserNode UserIn) =>
+            _Users.ReplaceOne(user => user.Username == username, UserIn);
 
         public void Remove(UserNode userIn) =>
             _Users.DeleteOne(user => user.Username == userIn.Username);
 
-        public void Remove(string Username) =>
-            _Users.DeleteOne(user => user.Username == Username);
+        public void Remove(string username) => 
+            _Users.DeleteOne(user => user.Username == username);
+    
     }
 }
