f3b8250b13cd6ca5a9229a8a820806437a56b407
Mario Estuardo Gómez Orellana <mario.e.g.orellana@gmail.com>
2019-11-23 20:04:05 -0600

    Creación de estructuras de base de datos y configuración de appstart.json

---
+++ UsersAPI/Models/UserNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace UsersAPI.Models
+{
+    public class UserNode
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Username")]
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
--- UsersAPI/Startup.cs
+++ UsersAPI/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using UsersAPI.Models;
+using UsersAPI.Services;
 
 namespace UsersAPI
 {
@@ -24,6 +26,14 @@ namespace UsersAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<UsersDatabaseSettings>(
+            Configuration.GetSection(nameof(UsersDatabaseSettings)));
+
+            services.AddSingleton<IUsersDatabaseSettings>(sp =>
+            sp.GetRequiredService<IOptions<UsersDatabaseSettings>>().Value);
+            
+            services.AddSingleton<UsersService>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
